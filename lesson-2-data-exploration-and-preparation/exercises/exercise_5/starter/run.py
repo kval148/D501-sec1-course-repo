c929c03dcddbbdee624f3e8887ac352351e01d35
Katherine Austin <kval148@wgu.edu>
2026-02-10 12:33:31 -0700

    Completed L02E05

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import logging
+import os
 import pandas as pd
 import wandb
 
@@ -11,10 +12,53 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(project="exercise_5", job_type="process_data")
+    # Create/Start W&B run
+    run = wandb.init(project="exercise_5",
+        group="attempts",
+        job_type="process_data"
+        )
 
-    ## YOUR CODE HERE
-    pass
+    # Download input artifact
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+
+    # Read artifact into pandas DataFrame
+    df = pd.read_parquet(artifact_path)
+
+    # Drop duplicates
+    logger.info("Dropping duplicates")
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    # Add new text feature
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    # Save data to csv
+    outfile = args.artifact_name
+    df.to_csv(outfile)
+
+    # Create output artifact
+    output_artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description
+        )
+    
+    # Attach data to output artifact
+    output_artifact.add_file(outfile)
+
+    # Upload artifact to W&B
+    logger.info("Logging artifact")
+    run.log_artifact(output_artifact)
+
+    # Remove output data file
+    os.remove(outfile)
+
+    # Don't need to call run.finish() because when there is
+    # only 1 W&B run in a script, it is automatically closed
+    # when the script finishes
 
 
 if __name__ == "__main__":
@@ -31,11 +75,17 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--artifact_name", type=str, help="Name for the artifact", required=True
+        "--artifact_name",
+        type=str,
+        help="Name for the artifact",
+        required=True
     )
 
     parser.add_argument(
-        "--artifact_type", type=str, help="Type for the artifact", required=True
+        "--artifact_type",
+        type=str,
+        help="Type for the artifact",
+        required=True
     )
 
     parser.add_argument(
